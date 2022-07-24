a86aa1b0a76e5bc32f672e406ece61272a03a59f
John Grey <johngrey4296@gmail.com>
2022-07-24 18:40:22 +0100

    [parser] add initial pyparse parser

--- instal/parser/pyparse_institution.py
+++ instal/parser/pyparse_institution.py
@@ -1,7 +1,8 @@
-#/usr/bin/env python3
-"""
-
-"""
+#/usr/bin/env python3,
+""",
+,
+""",
+##-- imports
 from __future__ import annotations
 
 import abc
@@ -15,9 +16,62 @@ from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
                     cast, final, overload, runtime_checkable)
 from uuid import UUID, uuid1
 from weakref import ref
+import pyparsing as pp
 
 if TYPE_CHECKING:
     # tc only imports
     pass
+##-- end imports
 
+##-- logging
 logging = logmod.getLogger(__name__)
+##-- end logging
+
+##-- consts
+comparisons = ["==", "!=", "<" ">" "<=", ">="]
+##-- end consts
+
+##-- util
+s         = pp.Suppress
+op        = pp.Optional
+orm       = pp.OneOrMore
+zrm       = pp.ZeroOrMore
+kw        = pp.Keyword
+lit       = pp.Literal
+##-- end util
+
+
+comment    = pp.Regex(r"%.+?")
+semi       = lit(";")
+fluent_kws = [kw(x) for x in ["cross", "noninertial", "obligation"]]
+event_kws  = [kw(x) for x in ["exogenous", "inst", "violation"]]
+
+name = pp.Word(pp.alphanums)
+var  = pp.Word(pp.alphas.upper(), pp.alphanums)
+
+term      = pp.Forward()
+term_list = pp.delimited_list(term)
+
+term  <<= name + op(lit("(") + term_list + lit(")"))
+
+type_dec    = kw("type") + term + semi
+domain_spec = name + lit(":") + orm(name)
+
+fluent = op(pp.MatchFirst(fluent_kws)) + kw("fluent") + term + semi
+event  = op(pp.MatchFirst(event_kws))  + kw("event")  + term + semi
+
+condition = pp.Empty()
+relation  = (term
+             + pp.MatchFirst([kw(x) for x in ["generates", "initiates", "terminates",
+                                              "xgenerates", "xinitiates", "xterminates"]])
+             + term_list + op(condition) + semi)
+
+obligation = term + kw("when") + term_list + semi
+initially = kw("initially")    + term_list + semi
+
+bridge = kw("bridge") + term + semi
+sink   = kw("sink") + term + semi
+source = kw("source") + term + semi
+
+
+parse_point = None
--- instal/util/intermediate_rep.py
+++ instal/util/intermediate_rep.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 import abc
 import logging as logmod
 from copy import deepcopy
+from enum import Enum, auto
 from dataclasses import InitVar, dataclass, field
 from re import Pattern
 from typing import (TYPE_CHECKING, Any, Callable, ClassVar, Final, Generic,
@@ -20,6 +21,40 @@ from weakref import ref
 
 logging = logmod.getLogger(__name__)
 
+##-- enums
+class EventEnum(Enum):
+    exogenous     = auto()
+    institutional = auto()
+    violation     = auto()
+    creation      = auto()
+    dissolution   = auto()
+    cross         = auto()
+    generates     = auto()
+    initiates     = auto()
+    terminates    = auto()
+    initially     = auto()
+
+
+class FluentEnum(Enum):
+    inertial    = auto()
+    noninertial = auto()
+    power       = auto()
+    permission  = auto()
+    violation   = auto()
+    obligation  = auto()
+    cross       = auto()
+
+class CrossEventEnum(Enum):
+    generates = auto()
+    initiates = auto()
+    terminates = auto()
+    xgenerates = auto()
+    xinitiates = auto()
+    xterminates = auto()
+
+##-- end enums
+
+##-- IRs
 @dataclass
 class InstalIR:
 
@@ -27,18 +62,72 @@ class InstalIR:
 
 @dataclass
 class IR_Program(InstalIR):
+    """
+    Combines institutions and bridges together
+    """
     institutions : list[InstalIR] = field(default_factory=list)
     bridges      : list[InstalIR] = field(default_factory=list)
 
 @dataclass
 class BridgeIR(InstalIR):
-    source : str = field()
-    sink   : str = field()
+    name      : TermIR             = field()
+    sources   : list[TermIR]       = field(default_factory=list)
+    sinks     : list[TermIR]       = field(default_factory=list)
+    relations : list[RelationalIR] = field(default_factory=list)
+    fluents   : list[FluentIR]     = field(default_factory=list)
 
 @dataclass
 class InstitutionIR(InstalIR):
-    name : str = field()
+    name      : TermIR             = field()
+    fluents   : list[InstalIR]     = field(default_factory=list)
+    events    : list[EventIR]      = field(default_factory=list)
+    types     : list[TypeIR]       = field(default_factory=list)
+    relations : list[RelationalIR] = field(default_factory=list)
 
-    fluents : list[InstalIR] = field()
-    events  : list[InstalIR] = field()
-    types   : list[InstalIR] = field()
+
+@dataclass
+class TermIR(InstalIR):
+    name   : str = field()
+    params : list[InstalIR] = field(default_factory=list)
+    is_var : bool = field(default=False)
+
+@dataclass
+class TypeIR(InstalIR):
+    name : TermIR = field()
+
+@dataclass
+class DomainSpecIR(InstalIR):
+    name  : TermIR       = field()
+    terms : list[TermIR] = field(default_factory=list)
+
+@dataclass
+class EventIR(InstalIR):
+    name    : TermIR    = field()
+    variety : EventEnum = field()
+
+@dataclass
+class FluentIR(InstalIR):
+    name    : TermIR     = field()
+    variety : FluentEnum = field()
+
+
+@dataclass
+class RelationalIR(InstalIR):
+    """
+    for generation and consequence relations
+    """
+    head       : TermIR         = field()
+    variety    : CrossEventEnum = field()
+    body       : list[TermIR]   = field(default_factory=list)
+    conditions : list[TermIR]   = field(default_factory=list)
+
+@dataclass
+class ObligationIR(InstalIR):
+    head : TermIR       = field()
+    body : list[TermIR] = field(default_factory=list)
+
+@dataclass
+class InitiallyIR(InstalIR):
+    body : list[TermIR] = field(default_factory=list)
+
+##-- end IRs
