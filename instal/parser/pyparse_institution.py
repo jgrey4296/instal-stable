f69c6b7c4dc1fd552f93704e47df115e474aa3ea
John Grey <johngrey4296@gmail.com>
2022-08-03 23:06:45 +0100

    [parsing] extend

--- instal/parser/pyparse_institution.py
+++ instal/parser/pyparse_institution.py
@@ -1,6 +1,6 @@
 #/usr/bin/env python3,
 """
-,
+
 """
 ##-- imports
 from __future__ import annotations
@@ -31,23 +31,59 @@ logging = logmod.getLogger(__name__)
 ##-- end logging
 
 ##-- util
-s         = pp.Suppress
-op        = pp.Optional
-orm       = pp.OneOrMore
-zrm       = pp.ZeroOrMore
-kw        = pp.Keyword
-lit       = pp.Literal
+s       = pp.Suppress
+op      = pp.Optional
+orm     = pp.OneOrMore
+zrm     = pp.ZeroOrMore
+kw      = pp.Keyword
+lit     = pp.Literal
+s_kw    = lambda x: pp.Keyword(x).suppress()
+s_lit   = lambda x: pp.Literal(x).suppress()
+comment = pp.Regex(r"%.+?")
+semi    = lit(";").suppress()
+
+fluent_kws   = pp.MatchFirst([kw(x) for x in ["cross", "noninertial", "obligation"]])
+event_kws    = pp.MatchFirst([kw(x) for x in ["exogenous", "inst", "violation"]])
+relation_kws = pp.MatchFirst([kw(x) for x in ["generates", "initiates", "terminates",
+                                              "xgenerates", "xinitiates", "xterminates"]])
 ##-- end util
 
 ##-- constructors
-def build_term(string, loc, toks):
-    is_var, head = toks['head']
+def build_institution(string, loc, toks):
+    inst = toks['head']
+    body = toks['body']
+    for elem in body:
+        match elem:
+            case ASTs.FluentAST():
+                inst.fluents.append(elem)
+            case ASTs.EventAST():
+                inst.events.append(elem)
+            case ASTs.TypeAST():
+                inst.types.append(elem)
+            case ASTs.RelationalAST():
+                inst.relations.append(elem)
+            case ASTs.NifRuleAST():
+                inst.nif_rules.append(elem)
+            case ASTs.InitiallyAST():
+                inst.initial.append(elem)
+            case ASTs.SourceAST():
+                inst.sources.append(elem.head)
+            case ASTs.SinkAST():
+                inst.sinks.append(elem.head)
+
+    return inst
+
+
+
+
+def build_term(string, loc, toks) -> ASTs.TermAST:
+    is_var, head = toks['head'][0]
     return ASTs.TermAST(head,
                       params=toks['params'][:] if 'params' in toks else [],
                       is_var=is_var)
 
 
-def build_fluent(string, loc, toks):
+def build_fluent(string, loc, toks) -> ASTs.FluentAST:
     head     = toks['head']
     anno_str = toks['annotation'] if 'annotation' in toks else None
     match anno_str:
@@ -57,12 +93,13 @@ def build_fluent(string, loc, toks):
             annotation = ASTs.FluentEnum.noninertial
         case "obligation":
             annotation = ASTs.FluentEnum.obligation
+            assert(len(head.params) == 3), "Obligation Fluents need a requirement, deadline, and violation"
         case _:
             annotation = None
 
     return ASTs.FluentAST(head, annotation)
 
-def build_event(string, loc, toks):
+def build_event(string, loc, toks) -> ASTs.EventAST:
     head     = toks['head']
     anno_str = toks['annotation'] if 'annotation' in toks else None
     match anno_str:
@@ -77,7 +114,7 @@ def build_event(string, loc, toks):
 
     return ASTs.EventAST(head, annotation)
 
-def build_relation(string, loc, toks):
+def build_relation(string, loc, toks) -> ASTs.RelationalAST:
     head       = toks['head']
     body       = toks['body'][:]
     conditions = toks['conditions'] if 'conditions' in toks else []
@@ -99,121 +136,155 @@ def build_relation(string, loc, toks):
             raise Exception("Not Recognised consequence relation: %s", toks['annotation'])
 
     return ASTs.RelationalAST(head,
-                            annotation,
-                            body,
-                            conditions)
+                              annotation,
+                              body,
+                              conditions)
 
 ##-- end constructors
 
 ##-- parser components
-comment      = pp.Regex(r"%.+?")
-semi         = lit(";").suppress()
-fluent_kws   = pp.MatchFirst([kw(x) for x in ["cross", "noninertial", "obligation"]])
-event_kws    = pp.MatchFirst([kw(x) for x in ["exogenous", "inst", "violation"]])
-relation_kws = pp.MatchFirst([kw(x) for x in ["generates", "initiates", "terminates",
-                                "xgenerates", "xinitiates", "xterminates"]])
-
 name = pp.Word(pp.alphanums)
 name.set_parse_action(lambda s, l, t: (False, t[0]))
 var       = pp.Word(pp.alphas.upper(), pp.alphanums)
 var.set_parse_action(lambda s, l, t: (True, t[0]))
 
 # The core Term parser:
-term      = pp.Forward()
-term_list = pp.delimited_list(term)
+TERM      = pp.Forward()
+term_list = pp.delimited_list(TERM)
 
-term  <<= (var | name)("head") + op(lit("(") + term_list("params") + lit(")"))
-term.set_parse_action(build_term)
-term.set_name("term")
+TERM  <<= (var | name)("head") + op(lit("(") + term_list("params") + lit(")"))
+TERM.set_parse_action(build_term)
+TERM.set_name("term")
 
 # Statements:
-type_dec    = kw("type").suppress() + term('head') + semi
-type_dec.set_parse_action(lambda s, l, t: ASTs.TypeAST(t['head']))
+TYPE_DEC    = s_kw("type") + TERM('head') + semi
+TYPE_DEC.set_parse_action(lambda s, l, t: ASTs.TypeAST(t['head']))
 
-domain_spec = term('head') + lit(":").suppress() + orm(term)("body")
-domain_spec.set_parse_action(lambda s, l, t: ASTs.DomainSpecAST(t['head'], t['body'][:]))
+FLUENT      = op(fluent_kws)("annotation") + s_kw("fluent")+ TERM("head") + semi
+FLUENT.set_parse_action(build_fluent)
 
-fluent = op(fluent_kws)("annotation") + kw("fluent").suppress() + term("head") + semi
-fluent.set_parse_action(build_fluent)
+EVENT       = op(event_kws)('annotation')  + s_kw("event")  + TERM("head") + semi
+EVENT.set_parse_action(build_event)
 
-event  = op(event_kws)('annotation')  + kw("event").suppress()  + term("head") + semi
-event.set_parse_action(build_event)
+# TODO handle comparisons
+CONDITION   = op(kw("not"))("not") + TERM("head")
+CONDITION.set_parse_action(lambda s, l, t: ASTs.ConditionAST(t['head'], True if 'not' in t else False))
 
-condition = op(kw("not"))("not") + term("head")
-condition.set_parse_action(lambda s, l, t: ASTs.ConditionAST(t['head'], True if 'not' in t else False))
+CONDITIONS  = s_kw("if") + pp.delimited_list(CONDITION)
+# TODO handle 'in {time}'
 
-conditions = kw("if").suppress() + pp.delimited_list(condition)
+RELATION    = (TERM("head")
+               + relation_kws("annotation")
+               + term_list("body") + op(CONDITIONS)("conditions") + semi)
+RELATION.set_parse_action(build_relation)
 
-relation  = (term("head")
-            + relation_kws("annotation")
-            + term_list("body") + op(conditions)("conditions") + semi)
-relation.set_parse_action(build_relation)
+NIF_RULE  = TERM("head") + s_kw("when") + term_list("body") + semi
+NIF_RULE.set_parse_action(lambda s, l, t: ASTs.NifRuleAST(t['head'], t['body'][:]))
 
-obligation = term("head") + kw("when").suppress() + term_list("body") + semi
-obligation.set_parse_action(lambda s, l, t: ASTs.ObligationAST(t['head'], t['body'][:]))
+INITIALLY   = s_kw("initially") + term_list("body") + op(CONDITIONS)("conditions") + semi
+INITIALLY.set_parse_action(lambda s, l, t: ASTs.InitiallyAST(t['body'][:], t['conditions'][:]))
 
-initially = kw("initially").suppress() + term_list("body") + semi
-initially.set_parse_action(lambda s, l, t: ASTs.InitiallyAST(t['body'][:]))
+INSTITUTION = s_kw("institution") + TERM("head") + semi
+INSTITUTION.set_parse_action(lambda s, l, t: ASTs.InstitutionDefAST(t['head']))
 
-institution = kw("institution").suppress() + term("head") + semi
-institution.set_parse_action(lambda s, l, t: ASTs.InstitutionDefAST(t['head']))
+BRIDGE      = s_kw("bridge") + TERM("head") + semi
+BRIDGE.set_parse_action(lambda s, l, t: ASTs.BridgeDefAST(t['head']))
 
-bridge = kw("bridge").suppress() + term("head") + semi
-bridge.set_parse_action(lambda s, l, t: ASTs.BridgeDefAST(t['head']))
+SINK        = s_kw("sink") + TERM("head") + semi
+SINK.set_parse_action(lambda s, l, t: ASTs.SinkAST(t['head']))
 
-sink   = kw("sink").suppress() + term("head") + semi
-sink.set_parse_action(lambda s, l, t: ASTs.SinkAST(t['head']))
-
-source = kw("source").suppress() + term("head") + semi
-source.set_parse_action(lambda s, l, t: ASTs.SourceAST(t['head']))
+SOURCE      = s_kw("source") + TERM("head") + semi
+SOURCE.set_parse_action(lambda s, l, t: ASTs.SourceAST(t['head']))
 ##-- end parser components
 
+##-- idc domain
+DOMAIN_SPEC = TERM('head') + s_lit(":") + orm(TERM)("body") + pp.line_end
+DOMAIN_SPEC.set_parse_action(lambda s, l, t: ASTs.DomainSpecAST(t['head'], t['body'][:]))
+##-- end idc domain
+
+##-- iaf facts
+IAF_INITIALLY   = s_kw("initially") + term("body") + op(CONDITIONS)("conditions") + s_kw('in') + term('inst') + pp.line_end
+IAF_INITIALLY.set_parse_action(lambda s, l, t: ASTs.InitiallyAST([t['body']], t['conditions'][:], inst=t['inst']))
+##-- end iaf facts
+
+##-- iaq query specification
+OBSERVED = s_kw('observed') + term('fact') + op(term)('inst') + op(s_kw('at') + pp.common.integer('time')) + pp.line_end
+OBSERVED.set_parse_action(lambda s, l, t: ASTs.QueryAST(t['fact'], inst=t['inst'], time=t['time']))
+##-- end iaq query specification
+
 ##-- top level parser entry points
-top_institution = (institution
-                   + orm(type_dec
-                         | event
-                         | fluent
-                         | relation
-                         | initially))
+top_institution = (INSTITUTION('head')
+                   + orm(TYPE_DEC
+                         | EVENT
+                         | FLUENT
+                         | RELATION
+                         | NIF_RULE
+                         | INITIALLY)('body'))
 top_institution.ignore(comment)
+top_institution.set_parse_action(build_institution)
 
-top_bridge = (bridge
-              + orm(source
-                    | sink
-                    | type_dec
-                    | event
-                    | fluent
-                    | relation
-                    | initially))
+top_bridge = (BRIDGE('head')
+              + orm(SOURCE
+                    | SINK
+                    | TYPE_DEC
+                    | EVENT
+                    | FLUENT
+                    | RELATION
+                    | NIF_RULE
+                    | INITIALLY)('body'))
 top_bridge.ignore(comment)
+top_bridge.set_parse_action(build_institution)
 
-top_fact = orm(initially)
+top_fact = orm(IAF_INITIALLY)
 top_fact.ignore(comment)
+top_fact.set_parse_action(lambda s, l, t: ASTs.FactTotalityAST(t[:]))
 
-top_atom = orm(term)
+top_query = orm(OBSERVED)
+top_query.ignore(comment)
+top_query.set_parse_action(lambda s, l, t: ASTs.QueryTotalityAST(t[:]))
+
+top_domain = orm(DOMAIN_SPEC)
+top_domain.ignore(comment)
+top_domain.set_parse_action(lambda s, l, t: ASTs.DomainTotalityAST(t[:]))
 ##-- end top level parser entry points
 
 ##-- interface implementation
 class InstalPyParser(InstalParser):
 
 
-    def parse_institution(self, text:str) -> ASTs.InstalAST:
+    def parse_institution(self, text:str, *, source:str=None) -> ASTs.InstitutionDefAST:
         """ Mainly for .ial's """
-        # TODO group results
-        return top_institution.parse_string(text, parse_all=True)[0]
+        result = top_institution.parse_string(text, parse_all=True)[0]
+        if source is not None:
+            result.source = source
+        return result
 
-    def parse_bridge(self, text:str) -> ASTs.InstalAST:
+    def parse_bridge(self, text:str, *, source:str=None) -> ASTs.BridgeDefAST:
         """ Mainly for .iab's """
-        # TODO group results
-        return top_brige.parse_string(text, parse_all=True)[0]
+        result = top_brige.parse_string(text, parse_all=True)[0]
+        if source is not None:
+            result.source = source
+        return result
 
-    def parse_facts(self, text:str) -> list[ASTs.InstalAST]:
+    def parse_domain(self, text:str, *, source:str=None) -> ASTs.DomainTotalityAST:
+        """ For .idc's """
+        result = top_domain.parse_string(text, parse_all=True)[0]
+        if source is not None:
+            result.source = source
+        return result
+
+    def parse_situation(self, text:str, *, source:str=None) -> ASTs.FactTotalityAST:
         """ Mainly for .iaf's """
-        return top_fact.parse_string(text, parse_all=True)[:]
+        result = top_fact.parse_string(text, parse_all=True)[0]
+        if source is not None:
+            result.source = source
+        return result
 
-    def parse_atoms(self, text:str) -> list[ASTs.InstalAST]:
-        """ Mainly for parsing json fragment for traces """
-        return term.parse_string(text, parse_all=True)[:]
-
+    def parse_query(self, text:str, *, source:str=None) -> ASTs.QueryTotalityAST:
+        """ Mainly for .iaq's """
+        result = top_query.parse_string(text, parse_all=True)[0]
+        if source is not None:
+            result.source = source
+        return result
 
 ##-- end interface implementation
